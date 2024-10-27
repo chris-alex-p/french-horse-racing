f491d4c4cf893e0b19a96adf7e33ef8c3711c77d
chris-alex-p <140163053+chris-alex-p@users.noreply.github.com>
2024-10-27 23:29:52 +0100

    Create french_trotting_2019_analysis_data_extraction.sql

---
+++ sql_scripts/queries/french_trotting_2019_analysis_data_extraction.sql
@@ -0,0 +1,28 @@
+SELECT 
+  r.race_json -> 'reunion' ->> 'date_reunion' AS date_reunion,
+  r.race_json -> 'reunion' -> 'hippodrome' ->> 'name' AS hippodrome,
+  r.race_json ->> 'lib_piste_course' AS piste_course,
+  r.race_json ->> 'lib_corde_course' AS corde_course,
+  r.race_json ->> 'num_course_pmu' AS num_course_pmu,
+  r.race_json ->> 'liblong_prix_course' AS liblong_prix_course,
+  r.race_json ->> 'discipline' AS discipline, 
+  r.race_json ->> 'distance' AS distance,
+  r.race_json ->> 'typ_dep_course' AS depart,
+  r.race_json ->> 'lib_parcours_course' AS parcours_course,
+  r.race_json ->> 'type_course' AS type_course,
+  r.race_json ->> 'categ_course' AS categ_course,
+  r.race_json ->> 'montant_total_allocation' AS total_allocation,
+  r.race_json ->> 'conditions_txt_course' AS conditions_txt_course,
+  j -> 'cheval' ->> 'nom_cheval' AS nom_cheval,
+  j -> 'monte' ->> 'nom_monte' AS nom_driver,
+  j -> 'reduction_km' AS reduction_km,
+  j ->> 'texte_place_arrivee' AS texte_place_arrivee,
+    j ->> 'num_place_arrivee' AS num_place_arrivee,
+  o ->> 'rapp_evol' AS odds
+FROM raw_data.races_raw_data AS r
+CROSS JOIN jsonb_array_elements((r.race_json ->> 'partants')::jsonb) AS j
+LEFT JOIN LATERAL jsonb_array_elements((r.race_json ->> 'odds')::jsonb) AS o 
+ON j -> 'cheval' ->> 'nom_cheval' = o -> 'cheval' ->> 'nom_cheval'
+WHERE r.race_json ->> 'discipline' IN ('Attelé', 'Monté')
+  AND r.race_json -> 'reunion' -> 'hippodrome' ->> 'country_code' = 'FRA'
+  AND r.race_json -> 'reunion' ->> 'date_reunion' LIKE '2019%'; 
